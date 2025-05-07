3f44c8d46463c15f51e1b2efbf92d1ddd18b8248
SEONGYEON JEUNG <godricj1@gmail.com>
2025-05-07 16:33:12 +0900

    feat: neo4j repository test

--- backend/src/main/java/com/grepp/smartwatcha/infra/neo4j/node/WriterNode.java
+++ backend/src/main/java/com/grepp/smartwatcha/infra/neo4j/node/WriterNode.java
@@ -1,11 +1,15 @@
 package com.grepp.smartwatcha.infra.neo4j.node;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 
 @Node("WRITER")
 @RequiredArgsConstructor
+@Getter @Setter @ToString
 public class WriterNode {
 
     @Id
