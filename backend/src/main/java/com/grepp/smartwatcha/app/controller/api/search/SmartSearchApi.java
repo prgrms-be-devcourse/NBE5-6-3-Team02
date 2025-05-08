ebedfe444f9c550ff9cf5aa73c3228e7f5225a92
SEONGYEON JEUNG <godricj1@gmail.com>
2025-05-08 15:02:52 +0900

    fix: transaction manager conflict & feat: smartSearch Api

---
+++ backend/src/main/java/com/grepp/smartwatcha/app/controller/api/search/SmartSearchApi.java
@@ -0,0 +1,22 @@
+package com.grepp.smartwatcha.app.controller.api.search;
+
+import com.grepp.smartwatcha.app.controller.api.search.payload.SmartSearchApiRequest;
+import com.grepp.smartwatcha.app.controller.api.search.payload.SmartSearchResponse;
+import com.grepp.smartwatcha.infra.config.FeignCommonConfig;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(
+        name = "smart-search-api",
+        url = "http://localhost:8000/llm/search",
+        configuration = {FeignCommonConfig.class})
+public interface SmartSearchApi {
+
+    @PostMapping(consumes = "application/json")
+    SmartSearchResponse call(
+            @RequestHeader("X-Internal-Token") String xInternalToken,
+            @RequestBody SmartSearchApiRequest request
+            );
+}
--- backend/src/main/java/com/grepp/smartwatcha/infra/neo4j/repository/MovieNeo4jRepository.java
+++ backend/src/main/java/com/grepp/smartwatcha/infra/neo4j/repository/MovieNeo4jRepository.java
@@ -4,7 +4,7 @@ import com.grepp.smartwatcha.infra.neo4j.node.MovieNode;
 import java.util.Optional;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
-public interface MovieRepository extends Neo4jRepository<MovieNode, Long> {
+public interface MovieNeo4jRepository extends Neo4jRepository<MovieNode, Long> {
 
     Optional<MovieNode> findByTitle(String title);
 }
