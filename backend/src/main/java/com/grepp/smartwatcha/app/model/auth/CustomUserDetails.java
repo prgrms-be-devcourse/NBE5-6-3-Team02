9fe1cd5c1bc032a1d6566f443b45f2d7ea8a3dd2
endorsement0912 <ef032550@naver.com>
2025-05-08 15:21:17 +0900

    Feat: UserEntity 기반의 UserDetails 구현

---
+++ backend/src/main/java/com/grepp/smartwatcha/app/model/auth/CustomUserDetails.java
@@ -0,0 +1,65 @@
+package com.grepp.smartwatcha.app.model.auth;
+
+import com.grepp.smartwatcha.infra.jpa.entity.UserEntity;
+import com.grepp.smartwatcha.infra.jpa.enums.Role;
+
+
+import java.util.Collection;
+import java.util.Collections;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomUserDetails implements UserDetails {
+
+  private final UserEntity user;
+
+  public CustomUserDetails(UserEntity user) {
+    this.user = user;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return Collections.singleton(
+        new SimpleGrantedAuthority("ROLE_" + user.getRole().name())
+    );
+  }
+
+  @Override
+  public String getPassword() {
+    return user.getPassword();
+  }
+
+  @Override
+  public String getUsername() {
+    return user.getEmail();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
+  public String getName() {
+    return user.getName();
+  }
+
+  public Role getRole() {
+    return user.getRole();
+  }
+}
