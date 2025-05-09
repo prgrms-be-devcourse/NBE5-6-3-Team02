39006d430500e154be4a6e4248efdbeaa7dbde2b
Jun <wnsur1234@gmail.com>
2025-05-09 13:15:53 +0900

    Chore: details패키지 구조 변경 및 클래스,메서드명 변경

--- backend/src/main/java/com/grepp/smartwatcha/app/details/service/RatingService.java
+++
@@ -1,8 +0,0 @@
-package com.grepp.smartwatcha.app.details.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class RatingService {
-
-}
--- backend/src/main/java/com/grepp/smartwatcha/infra/config/JpaConfig.java
+++ backend/src/main/java/com/grepp/smartwatcha/infra/config/JpaConfig.java
@@ -2,22 +2,15 @@ package com.grepp.smartwatcha.infra.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableJpaAuditing
-@EnableTransactionManagement
 public class JpaConfig {
 
     @PersistenceContext
@@ -26,11 +19,5 @@ public class JpaConfig {
     @Bean
     public JPAQueryFactory jpaQueryFactory(){
         return new JPAQueryFactory(entityManager);
-    }
-
-    @Bean(name = "transactionManager")
-    @Primary
-    public PlatformTransactionManager jpaTransactionManager(EntityManagerFactory emf) {
-        return new JpaTransactionManager(emf);
     }
 }
