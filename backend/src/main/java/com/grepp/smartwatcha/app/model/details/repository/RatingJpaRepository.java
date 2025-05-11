cd12ef29cb3ffdaecef93b69f66b6db9a87d0c90
Jun <wnsur1234@gmail.com>
2025-05-11 15:46:25 +0900

    Refoctor: 별점 저장을 위한 html 함수 및 thymeleaf를 이용한 기능 수정

--- backend/src/main/java/com/grepp/smartwatcha/app/model/details/repository/RatingJpaRepository.java
+++ backend/src/main/java/com/grepp/smartwatcha/app/model/details/repository/RatingJpaRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface RatingJpaRepository extends JpaRepository<RatingEntity, Long> {
-    List<RatingEntity> findAllByUserId(Long id);
+
 }
