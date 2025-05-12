92ebddb9bb881c4380f884989b3e58f8fa799968
endorsement0912 <ef032550@naver.com>
2025-05-12 19:47:57 +0900

    feat: 관리자 사용자 목록 응답 DTO AdminUserListResponse 생성

---
+++ backend/src/main/java/com/grepp/smartwatcha/app/model/admin/user/AdminUserListResponse.java
@@ -0,0 +1,24 @@
+package com.grepp.smartwatcha.app.model.admin.user;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class AdminUserListResponse {
+  private Long id;
+  private String name;
+  private String email;
+  private LocalDateTime createdAt;
+  private LocalDateTime modifiedAt;
+  private Boolean activated;
+  private String role;
+  private boolean updatedRecently;
+  private List<AdminSimpleRatingDto> recentRatings;
+}
