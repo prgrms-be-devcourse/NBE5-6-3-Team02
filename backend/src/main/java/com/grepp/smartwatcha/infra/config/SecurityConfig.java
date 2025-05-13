20c02ee3fb57983960d769870631629707ce2505
SEONGYEON JEUNG <godricj1@gmail.com>
2025-05-13 12:46:49 +0900

    fix: PasswordEncoder and login form parameter

--- backend/src/main/java/com/grepp/smartwatcha/infra/config/SecurityConfig.java
+++ backend/src/main/java/com/grepp/smartwatcha/infra/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -80,6 +81,6 @@ public class SecurityConfig {
 
   @Bean
   public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
   }
 }
