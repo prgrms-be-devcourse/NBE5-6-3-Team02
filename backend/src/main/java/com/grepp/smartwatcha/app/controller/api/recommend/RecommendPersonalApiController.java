09ef192835cbed876dc7f759c82859e5f0548017
youngjun <ghkddudwns12345@naver.com>
2025-05-15 09:15:11 +0900

    수정 완료

--- backend/src/main/java/com/grepp/smartwatcha/app/controller/api/recommend/RecommendPersonalApiController.java
+++ backend/src/main/java/com/grepp/smartwatcha/app/controller/api/recommend/RecommendPersonalApiController.java
@@ -3,6 +3,8 @@ package com.grepp.smartwatcha.app.controller.api.recommend;
 import com.grepp.smartwatcha.app.controller.api.recommend.payload.MovieRecommendPersonalResponse;
 import com.grepp.smartwatcha.app.model.recommend.RecommendPersonalMovieService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,15 +19,15 @@ public class RecommendPersonalApiController {
 
     private final RecommendPersonalMovieService recommendService;
 
-//    @GetMapping("/personal")
-//    public List<MovieRecommendPersonalResponse> getPersonalRecommendations(@AuthenticationPrincipal UserDetails userDetails) {
-//        Long userId = Long.parseLong(userDetails.getUsername());
-//        return recommendService.getTop10PersonalMovies(userId);
-//    }
-
     @GetMapping("/personal")
-    public List<MovieRecommendPersonalResponse> getPersonalRecommendations(@RequestParam Long userId) {
+    public List<MovieRecommendPersonalResponse> getPersonalRecommendations(@AuthenticationPrincipal UserDetails userDetails) {
+        Long userId = Long.parseLong(userDetails.getUsername());
         return recommendService.getTop10PersonalMovies(userId);
     }
+
+//    @GetMapping("/personal")
+//    public List<MovieRecommendPersonalResponse> getPersonalRecommendations(@RequestParam Long userId) {
+//        return recommendService.getTop10PersonalMovies(userId);
+//    }
 
 }
