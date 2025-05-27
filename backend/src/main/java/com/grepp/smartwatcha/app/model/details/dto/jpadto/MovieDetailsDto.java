09226b9f66fb553dfde2de7bd0d8010d5546c3c4
Jun <wnsur1234@gmail.com>
2025-05-27 17:37:08 +0900

    Rename: Dto 네이밍 컨밴션 수정 (DTo -> Dto)

--- backend/src/main/java/com/grepp/smartwatcha/app/model/details/dto/jpadto/MovieDetailsDto.java
+++ backend/src/main/java/com/grepp/smartwatcha/app/model/details/dto/jpadto/MovieDetailsDto.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class MovieDetailsDTO {
+public class MovieDetailsDto {
     private Long id;
     private String title;
     private LocalDateTime releaseDate;
