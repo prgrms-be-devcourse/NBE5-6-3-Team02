534321cd402b0418d7edc50592940b5be16cfa2e
youngjun <ghkddudwns12345@naver.com>
2025-05-29 16:08:25 +0900

    rebase 전 커밋

--- backend/src/main/java/com/grepp/smartwatcha/app/model/recommend/service/personal/RecommendPersonalRatedNeo4jService.java
+++ backend/src/main/java/com/grepp/smartwatcha/app/model/recommend/service/personal/RecommendPersonalRatedNeo4jService.java
@@ -1,11 +1,10 @@
-package com.grepp.smartwatcha.app.model.recommend.service;
+package com.grepp.smartwatcha.app.model.recommend.service.personal;
 
 import com.grepp.smartwatcha.app.controller.api.recommend.payload.MovieGenreTagResponse;
 import com.grepp.smartwatcha.app.model.recommend.repository.MovieGenreCustomNeo4jRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -13,9 +12,7 @@ import java.util.List;
 @Transactional(transactionManager = "neo4jTransactionManager", readOnly = true)
 public class RecommendPersonalRatedNeo4jService {
 
-
     private final MovieGenreCustomNeo4jRepository movieGenreCustomRepository;
-
 
     public List<MovieGenreTagResponse> getGenreTagInfoByMovieIdList(List<Long> movieIdList) {
         return movieGenreCustomRepository.findGenresAndTagsByMovieIdList(movieIdList);
