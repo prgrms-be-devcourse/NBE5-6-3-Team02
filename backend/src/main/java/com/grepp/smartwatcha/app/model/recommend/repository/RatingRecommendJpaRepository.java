d5a88a55a2d4fdc8aff4dea742a1e713f4748548
youngjun <ghkddudwns12345@naver.com>
2025-05-30 16:35:26 +0900

    Refactor 콘텐츠기반 필터링

--- backend/src/main/java/com/grepp/smartwatcha/app/model/recommend/repository/RatingRecommendJpaRepository.java
+++ backend/src/main/java/com/grepp/smartwatcha/app/model/recommend/repository/RatingRecommendJpaRepository.java
@@ -12,6 +12,7 @@ public interface RatingRecommendJpaRepository extends JpaRepository<RatingEntity
 
     List<RatingEntity> findByUserId(Long userId);
 
+    // 유저가 평가한 영화 중 2편 이상 평가한 다른 유저 id 조회
     @Query("""
         SELECT r.user.id
         FROM RatingEntity r
@@ -23,5 +24,6 @@ public interface RatingRecommendJpaRepository extends JpaRepository<RatingEntity
     List<Long> findUsersWithCommonRatedMovies(@Param("targetUserId") Long targetUserId,
                                               @Param("targetMovieIds") List<Long> targetMovieIds);
 
+    // 사용자들이 남긴 별점 조회
     List<RatingEntity> findByUserIdIn(List<Long> userIdList);
 }
