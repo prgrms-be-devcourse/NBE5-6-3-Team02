c6904b46de362cc2c249e32e72f05d395117ba44
Jun <wnsur1234@gmail.com>
2025-06-08 21:08:22 +0900

    Feat: 실제 시청 영화 날짜기록 Entity 추가

---
+++ backend/src/main/java/com/grepp/smartwatcha/infra/jpa/entity/MovieWatchedEntity.java
@@ -0,0 +1,30 @@
+package com.grepp.smartwatcha.infra.jpa.entity;
+
+import com.grepp.smartwatcha.infra.jpa.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "movie_watched")
+public class MovieWatchedEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id")
+    private MovieEntity movie;
+
+    private LocalDate watchDate;
+}
